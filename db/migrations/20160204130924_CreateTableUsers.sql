d233495729f95875f2f07af8203dbe6187570ddf
on99 <on99@users.noreply.github.com>
2016-02-06 14:50:56 +0800

    Refine create table users db migration

--- db/migrations/20160204130924_CreateTableUsers.sql
+++ db/migrations/20160204130924_CreateTableUsers.sql
@@ -5,14 +5,17 @@ CREATE TABLE `users` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL COMMENT 'unique email',
   `bitcoin_address` VARCHAR(63) NOT NULL COMMENT 'unique bitcoin address',
-  `udpated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `users` ADD UNIQUE INDEX (`email`), ADD UNIQUE INDEX (`bitcoin_address`);
+ALTER TABLE `users` 
+ADD UNIQUE INDEX (`email`), 
+ADD UNIQUE INDEX (`bitcoin_address`),
+ADD INDEX (`created_at`),
+ADD INDEX (`updated_at`);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
-ALTER TABLE `users` DROP INDEX `email`, DROP INDEX `bitcoin_address`;
 DROP TABLE `users`;
