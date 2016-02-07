edd4c3637c8cf41e1fc8335f218d5135b65f3dc9
on99 <on99@users.noreply.github.com>
2016-02-07 22:28:45 +0800

    Fine tune endpoints code and add sessions endpoint

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/freeusd/solebtc/Godeps/_workspace/src/github.com/gin-gonic/gin"
 	"github.com/freeusd/solebtc/controllers/v1"
 	"github.com/freeusd/solebtc/middlewares"
+	"github.com/freeusd/solebtc/services/mail/mandrill"
 	"github.com/freeusd/solebtc/services/storage/mysql"
 )
 
@@ -21,6 +22,9 @@ func main() {
 		panicWriter io.Writer = os.Stderr
 	)
 
+	// mailer
+	mailer := mandrill.New(config.Mandrill.Key, config.Mandrill.FromEmail, config.Mandrill.FromName)
+
 	// storage service
 	storage, err := mysql.New(config.DB.DataSourceName)
 	if err != nil {
@@ -31,11 +35,17 @@ func main() {
 	gin.SetMode(ginEnvMode())
 	router := gin.New()
 
-	router.Use(gin.RecoveryWithWriter(panicWriter))
-	router.Use(middlewares.LoggerWithWriter(logWriter))
-	router.Use(middlewares.CORS())
-	router.Use(middlewares.ErrorWriter())
+	// middlewares
+	recovery := gin.RecoveryWithWriter(panicWriter)
+	logger := middlewares.LoggerWithWriter(logWriter)
+	cors := middlewares.CORS()
+	errorWriter := middlewares.ErrorWriter()
+	authRequired := middlewares.AuthRequired(storage.GetAuthToken, config.AuthToken.Lifetime)
 
+	// globally use middlewares
+	router.Use(recovery).Use(logger).Use(cors).Use(errorWriter)
+
+	// version 1 api endpoints
 	v1Endpoints := router.Group("/v1")
 
 	// user endpoints
@@ -45,9 +55,11 @@ func main() {
 	// auth token endpoints
 	v1AuthTokenEndpoints := v1Endpoints.Group("/auth_tokens")
 	v1AuthTokenEndpoints.POST("", v1.Login(storage.GetUserByEmail, storage.CreateAuthToken))
-	v1AuthTokenEndpoints.
-		Use(middlewares.AuthRequired(storage.GetAuthToken, config.AuthToken.Lifetime)).
-		DELETE("", v1.Logout(storage.DeleteAuthToken))
+	v1AuthTokenEndpoints.Use(authRequired).DELETE("", v1.Logout(storage.DeleteAuthToken))
+
+	// session endpoints
+	v1SessionEndpoints := v1Endpoints.Group("/sessions")
+	v1SessionEndpoints.Use(authRequired).POST("", v1.RequestVerifyEmail(storage.GetUserByID, storage.UpsertSession, mailer.SendEmail))
 
 	fmt.Fprintf(logWriter, "SoleBTC is running on %s\n", config.HTTP.Port)
 	if err := router.Run(config.HTTP.Port); err != nil {
