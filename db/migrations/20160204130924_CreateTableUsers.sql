b17dd485aaa51a63228fe3c1d8fa7993685000e9
on99 <on99@users.noreply.github.com>
2016-02-07 16:30:03 +0800

    Migrate db add verified, last_email_sent_at to users

--- db/migrations/20160204130924_CreateTableUsers.sql
+++ db/migrations/20160204130924_CreateTableUsers.sql
@@ -5,6 +5,8 @@ CREATE TABLE `users` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL COMMENT 'unique email',
   `bitcoin_address` VARCHAR(63) NOT NULL COMMENT 'unique bitcoin address',
+  `verified` BOOL NOT NULL DEFAULT false COMMENT 'indicate if user verifies his email',
+  `last_email_sent_at` DATETIME NOT NULL DEFAULT '1000-01-01 00:00:00',
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -13,6 +15,8 @@ CREATE TABLE `users` (
 ALTER TABLE `users` 
 ADD UNIQUE INDEX (`email`), 
 ADD UNIQUE INDEX (`bitcoin_address`),
+ADD INDEX (`verified`),
+ADD INDEX (`last_email_sent_at`),
 ADD INDEX (`created_at`),
 ADD INDEX (`updated_at`);
 
--- models/user.go
+++ models/user.go
@@ -7,11 +7,13 @@ import (
 
 // User model
 type User struct {
-	ID             int       `db:"id"`
-	Email          string    `db:"email"`
-	BitcoinAddress string    `db:"bitcoin_address"`
-	UpdatedAt      time.Time `db:"updated_at"`
-	CreatedAt      time.Time `db:"created_at"`
+	ID              int       `db:"id"`
+	Email           string    `db:"email"`
+	BitcoinAddress  string    `db:"bitcoin_address"`
+	Verified        bool      `db:"verified"`
+	LastEmailSentAt time.Time `db:"last_email_sent_at"`
+	UpdatedAt       time.Time `db:"updated_at"`
+	CreatedAt       time.Time `db:"created_at"`
 }
 
 var _ json.Marshaler = User{}
@@ -19,7 +21,9 @@ var _ json.Marshaler = User{}
 // MarshalJSON implements json.Marshaler
 func (u User) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]interface{}{
-		"email":           u.Email,
-		"bitcoin_address": u.BitcoinAddress,
+		"email":              u.Email,
+		"bitcoin_address":    u.BitcoinAddress,
+		"verified":           u.Verified,
+		"last_email_sent_at": u.LastEmailSentAt,
 	})
 }
