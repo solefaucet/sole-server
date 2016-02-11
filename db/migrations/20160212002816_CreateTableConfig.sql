149af9341860f4d92f30d03a82070aba646b2614
on99 <on99@users.noreply.github.com>
2016-02-12 00:33:04 +0800

    Migrate db create table configs

---
+++ db/migrations/20160212002816_CreateTableConfig.sql
@@ -0,0 +1,15 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE `configs` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `total_reward_threshold` INT(11) NOT NULL COMMENT 'threshold that indicates reward_rate_type',
+  `referer_reward_rate` FLOAT NOT NULL COMMENT 'referer reward rate means the percentage get from reward of referee',
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE `configs`;
