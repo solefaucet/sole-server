03e1c284becdbe1d75f03f90eb586cae1e769156
on99 <on99@users.noreply.github.com>
2016-02-12 22:26:11 +0800

    Remove IncrementUserBalance from Storage interface

--- services/storage/mysql/user_test.go
+++ services/storage/mysql/user_test.go
@@ -3,7 +3,6 @@ package mysql
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	. "github.com/freeusd/solebtc/Godeps/_workspace/src/github.com/smartystreets/goconvey/convey"
 	"github.com/freeusd/solebtc/errors"
@@ -148,55 +147,5 @@ func TestUpdateUser(t *testing.T) {
 
 	withClosedConn(t, "When update user", func(s Storage) *errors.Error {
 		return s.UpdateUser(models.User{})
-	})
-}
-
-func TestIncrementUserBalance(t *testing.T) {
-	Convey("Given mysql storage with user data", t, func() {
-		s := prepareDatabaseForTesting()
-		s.CreateUser(models.User{Email: "e", BitcoinAddress: "b"})
-
-		Convey("When increment user balance with rewarded_at", func() {
-			now := time.Now()
-			err := s.IncrementUserBalance(1, 10, &now)
-			user, _ := s.GetUserByID(1)
-
-			Convey("Error should be nil", func() {
-				So(err, ShouldBeNil)
-			})
-
-			Convey("Users should be equal", func() {
-				So(user, func(actual interface{}, expected ...interface{}) string {
-					u := actual.(models.User)
-					if u.Balance == 10 && u.RewardedAt.Unix() == now.Unix() {
-						return ""
-					}
-					return fmt.Sprintf("User %v is unexpected", u)
-				})
-			})
-		})
-
-		Convey("When increment user balance without rewarded_at", func() {
-			err := s.IncrementUserBalance(1, 10, nil)
-			user, _ := s.GetUserByID(1)
-
-			Convey("Error should be nil", func() {
-				So(err, ShouldBeNil)
-			})
-
-			Convey("Users should be equal", func() {
-				So(user, func(actual interface{}, expected ...interface{}) string {
-					u := actual.(models.User)
-					if u.Balance == 10 {
-						return ""
-					}
-					return fmt.Sprintf("User %v is unexpected", u)
-				})
-			})
-		})
-	})
-
-	withClosedConn(t, "When increment user balance", func(s Storage) *errors.Error {
-		return s.IncrementUserBalance(0, 0, nil)
 	})
 }
