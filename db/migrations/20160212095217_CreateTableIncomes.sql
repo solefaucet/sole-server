a81a6be2b9776ac59acafa2a7aae91eea8f48016
on99 <on99@users.noreply.github.com>
2016-02-12 10:13:01 +0800

    Migrate db rename table rewards to incomes

--- db/migrations/20160212095217_CreateTableIncomes.sql
+++ db/migrations/20160212095217_CreateTableIncomes.sql
@@ -1,18 +1,23 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-CREATE TABLE `rewards` (
+CREATE TABLE `incomes` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `user_id` INT(11) NOT NULL,
-  `reward` INT(11) NOT NULL COMMENT 'reward in 1 Satonish',
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `referer_id` INT(11) NOT NULL,
+  `type` INT(2) NOT NULL COMMENT '0: reward, 1: offerwall',
+  `income` INT(11) NOT NULL,
+  `referer_income` INT(11) NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `rewards`
+ALTER TABLE `incomes`
 ADD INDEX (`user_id`),
+ADD INDEX (`referer_id`),
+ADD INDEX (`type`),
 ADD INDEX (`created_at`);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
-DROP TABLE `rewards`;
+DROP TABLE `incomes`;
