d0c0af64c39ffcf6e62a53ca3c83e238bdea5888
on99 <on99@users.noreply.github.com>
2016-05-07 22:02:03 +0800

    Update logger middleware that uses logrus

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -1,106 +1,42 @@
 package middlewares
 
 import (
-	"log"
+	"net/http"
 	"time"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
 )
 
-// copy from gin source code
-//
-var (
-	green   = string([]byte{27, 91, 57, 55, 59, 52, 50, 109})
-	white   = string([]byte{27, 91, 57, 48, 59, 52, 55, 109})
-	yellow  = string([]byte{27, 91, 57, 55, 59, 52, 51, 109})
-	red     = string([]byte{27, 91, 57, 55, 59, 52, 49, 109})
-	blue    = string([]byte{27, 91, 57, 55, 59, 52, 52, 109})
-	magenta = string([]byte{27, 91, 57, 55, 59, 52, 53, 109})
-	cyan    = string([]byte{27, 91, 57, 55, 59, 52, 54, 109})
-	reset   = string([]byte{27, 91, 48, 109})
-)
-
-// LoggerWithLogger instance a Logger middleware with a *log.Logger
-func LoggerWithLogger(out *log.Logger, notlogged ...string) gin.HandlerFunc {
-	var skip map[string]struct{}
-
-	if length := len(notlogged); length > 0 {
-		skip = make(map[string]struct{}, length)
-
-		for _, path := range notlogged {
-			skip[path] = struct{}{}
-		}
-	}
-
+// Logger returns a middleware that logs all request
+func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Start timer
+		// mark start time
 		start := time.Now()
-		path := c.Request.URL.Path
 
-		// Process request
+		// process request
 		c.Next()
 
-		// Log only when path is not being skipped
-		if _, ok := skip[path]; !ok {
-			// Stop timer
-			end := time.Now()
-			latency := end.Sub(start)
-
-			clientIP := c.ClientIP()
-			method := c.Request.Method
-			statusCode := c.Writer.Status()
-			statusColor := colorForStatus(statusCode)
-			methodColor := colorForMethod(method)
-
-			out.Printf("|%s %3d %s| %13v | %s |%s  %s %-7s %s\n",
-				statusColor, statusCode, reset,
-				latency,
-				clientIP,
-				methodColor, reset, method,
-				path,
-			)
-
-			lastError := c.Errors.ByType(gin.ErrorTypePrivate).Last()
-			if lastError != nil {
-				// v := lastError.Err.(*errors.Error).ErrStringForLogging
-				// if v != "" {
-				// 	out.Printf("%s\n", v)
-				// }
-			}
+		// log after processing
+		status := c.Writer.Status()
+		fields := logrus.Fields{
+			"method":           c.Request.Method,
+			"path":             c.Request.URL.Path,
+			"query":            c.Request.URL.Query().Encode(),
+			"http_status_code": status,
+			"response_time":    time.Since(start),
+			"ip":               c.ClientIP(),
 		}
-	}
-}
+		if err := c.Errors.ByType(gin.ErrorTypeAny).Last(); err != nil {
+			fields["error"] = err
+		}
 
-func colorForStatus(code int) string {
-	switch {
-	case code >= 200 && code < 300:
-		return green
-	case code >= 300 && code < 400:
-		return white
-	case code >= 400 && code < 500:
-		return yellow
-	default:
-		return red
-	}
-}
+		entry := logrus.WithFields(fields)
+		if status == http.StatusInternalServerError {
+			entry.Error("http error")
+			return
+		}
 
-func colorForMethod(method string) string {
-	switch method {
-	case "GET":
-		return blue
-	case "POST":
-		return cyan
-	case "PUT":
-		return yellow
-	case "DELETE":
-		return red
-	case "PATCH":
-		return green
-	case "HEAD":
-		return magenta
-	case "OPTIONS":
-		return white
-	default:
-		return reset
+		entry.Info("http access")
 	}
 }
