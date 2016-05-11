1456aa98cb122e5b35d54cf0885e06d292f72988
on99 <on99@users.noreply.github.com>
2016-05-11 22:23:33 +0800

    Fix broken test

--- handlers/v1/helper_test.go
+++ handlers/v1/helper_test.go
@@ -3,11 +3,11 @@ package v1
 import "testing"
 
 func TestMax(t *testing.T) {
-	if v := max(1, 2); v != 2 {
-		t.Errorf("Max(1, 2) should be 2 but get %v", v)
+	if v := min(1, 2); v != 1 {
+		t.Errorf("Min(1, 2) should be 1 but get %v", v)
 	}
 
-	if v := max(2, 1); v != 2 {
-		t.Errorf("Max(1, 2) should be 2 but get %v", v)
+	if v := min(2, 1); v != 1 {
+		t.Errorf("Min(1, 2) should be 1 but get %v", v)
 	}
 }
