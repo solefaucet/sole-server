ff18792dc4b688a9064df78286714dc9eb666969
on99 <on99@users.noreply.github.com>
2016-06-12 20:26:36 +0800

    Lock when broadcast and remove errored conns

--- services/hub/list/list.go
+++ services/hub/list/list.go
@@ -31,28 +31,7 @@ func (h *Hub) PutConn(c hub.Conn) {
 
 // Broadcast broadcast message to all connections
 func (h *Hub) Broadcast(m []byte) {
-	go func() {
-		// construct slice of connections
-		elems := h.elems()
-
-		// broadcast message
-		// collect errored connections
-		errElems := []*list.Element{}
-		wg := &sync.WaitGroup{}
-		for i := range elems {
-			wg.Add(1)
-			go func(e *list.Element) {
-				defer wg.Done()
-				if err := e.Value.(hub.Conn).Write(m); err != nil {
-					errElems = append(errElems, e)
-				}
-			}(elems[i])
-		}
-		wg.Wait()
-
-		// remove errored connections
-		h.removeErrElems(errElems)
-	}()
+	go h.broadcast(m)
 }
 
 // Len returns number of active connections
@@ -62,25 +41,18 @@ func (h *Hub) Len() int {
 	return h.conns.Len()
 }
 
-// iterate list, construct slice of connections
-func (h *Hub) elems() []*list.Element {
-	h.rwlock.RLock()
-	defer h.rwlock.RUnlock()
-	elems := make([]*list.Element, h.conns.Len())
-	for e, i := h.conns.Front(), 0; e != nil; e, i = e.Next(), i+1 {
-		elems[i] = e
-	}
-
-	return elems
-}
-
-// remove errored connections from list
-func (h *Hub) removeErrElems(errElems []*list.Element) {
+// broadcast and remove errored connections
+func (h *Hub) broadcast(raw []byte) {
 	h.rwlock.Lock()
 	defer h.rwlock.Unlock()
 
-	for i := range errElems {
-		errElems[i].Value.(hub.Conn).Close()
-		h.conns.Remove(errElems[i])
+	var next *list.Element
+	for e := h.conns.Front(); e != nil; e = next {
+		next = e.Next()
+		conn := e.Value.(hub.Conn)
+		if err := conn.Write(raw); err != nil {
+			conn.Close()
+			h.conns.Remove(e)
+		}
 	}
 }
