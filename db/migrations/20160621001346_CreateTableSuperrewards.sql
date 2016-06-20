b44b9d33de9b239c3435fbc69f876eafe5635b4e
on99 <on99@users.noreply.github.com>
2016-06-21 00:19:15 +0800

    Migrate db add superrewards table

---
+++ db/migrations/20160621001346_CreateTableSuperrewards.sql
@@ -0,0 +1,23 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE `superrewards` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `income_id` INT(11) NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  `transaction_id` VARCHAR(255) NOT NULL,
+  `offer_id` VARCHAR(127) NOT NULL,
+  `amount` DECIMAL(19, 8) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `superrewards`
+ADD UNIQUE INDEX (`income_id`),
+ADD UNIQUE INDEX (`user_id`, `transaction_id`),
+ADD INDEX (`offer_id`),
+ADD INDEX (`created_at`);
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE `superrewards`;
--- models/offerwall.go
+++ models/offerwall.go
@@ -10,3 +10,14 @@ type OfferwowEvent struct {
 	Amount    float64   `db:"amount"`
 	CreatedAt time.Time `db:"created_at"`
 }
+
+// SuperrewardsOffer model
+type SuperrewardsOffer struct {
+	ID            int64     `db:"id"`
+	IncomeID      int64     `db:"income_id"`
+	UserID        int64     `db:"user_id"`
+	TransactionID string    `db:"transaction_id"`
+	Amount        float64   `db:"amount"`
+	OfferID       string    `db:"offer_id"`
+	CreatedAt     time.Time `db:"created_at"`
+}
