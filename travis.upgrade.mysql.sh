26d779a9a46ab5df6c045a9d1da17235ea675a1e
on99 <on99@users.noreply.github.com>
2016-10-15 20:37:42 +0800

    Update install mysql5.7 script

--- travis.upgrade.mysql.sh
+++ travis.upgrade.mysql.sh
@@ -1,5 +1,6 @@
-echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
+echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | sudo debconf-set-selections
+wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
+sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
+sudo mysql_upgrade
